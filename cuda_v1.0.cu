616b594d90ca5a41904f7b75ca5d54e5a5001f72
SemyonIlyasov <s.ilyasov@g.nsu.ru>
2022-03-24 04:49:49 +0000

    cuda_v1.0

--- cuda_v1.0.cu
+++ cuda_v1.0.cu
@@ -1,30 +1,36 @@
 #include <stdio.h>
-
-#define N 128
-__global__ void five_point_model_calc(double* U_d, double* U_d_n, const int N){
+#include <stdlib.h>
+#include <math.h>
+//#include "cub/cub.cuh"
+#define N 10
+__global__ void five_point_model_calc(double* U_d, double* U_d_n, int n)
+{
+	
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if(i < N - 1 && i > 0)
+	if(i < n - 1 && i > 0)
 	{
 		int j = blockIdx.y * blockDim.y + threadIdx.y;
-		if (j < N - 1 && j > 0)
+		if (j < n - 1 && j > 0)
 		{
-			float left = U_d[i*N + j - 1];
-			float right = U_d[i*N + j + 1];
-			float up = U_d[(i-1)*N + j];
-			float down = U_d[(i+1)*N + j];
+			float left = U_d[i*n + j - 1];
+			float right = U_d[i*n + j + 1];
+			float up = U_d[(i-1)*n + j];
+			float down = U_d[(i+1)*n + j];
 
-			U_d_n[i*N + j] = 0.25 * (left + right + up + down);
+			U_d_n[i*n + j] = 0.25 * (left + right + up + down);
 		}
 	}
 }
 
-int main(void){
+int main(void)
+{
 
 double* U = (double*)malloc(N*N*sizeof(double));
-double* U_n = (double*)malloc(N*N*sizeof(double));
-double* U_d = (double*)malloc(N*N*sizeof(double));
-double* U_d_n = (double*)malloc(N*N*sizeof(double));
+double* U_n =(double*)malloc(N*N*sizeof(double));
+
+double* U_d;
+double* U_d_n;
 
 cudaMalloc(&U_d, sizeof(double)*N*N);
 cudaMalloc(&U_d_n, sizeof(double)*N*N);
@@ -43,12 +49,21 @@ for (int i = 0; i < N; i++)
 	U_n[(N-1)*N + i] = U[(N-1)*N + i];
 	U_n[i*N + N - 1] = U[i*N + N - 1];
 }
-
+cudaMemcpy(U_d, U, N*N*sizeof(double), cudaMemcpyHostToDevice);
 cudaMemcpy(U_d_n, U_n, N*N*sizeof(double), cudaMemcpyHostToDevice);
 
-dim3 BLOCK_SIZE = dim3(ceil(N/128), 1, 1);
-dim3 GRID_SIZE = dim3(128, 1, 1);
-five_point_model_calc<<<BLOCK_SIZE, GRID_SIZE>>>(U_d, U_d_n, N);
+dim3 GRID_SIZE = dim3(ceil((N + 127)/128.), 1, 1);
+dim3 BLOCK_SIZE = dim3(128, 1, 1);
+
+five_point_model_calc<<<GRID_SIZE, BLOCK_SIZE>>>(U_d, U_d_n, N);
+
+cudaMemcpy(U_n, U_d_n, N*N*sizeof(double), cudaMemcpyDeviceToHost);
+
+for(int i = 0; i < N; i++){
+	for(int j = 0; j < N; j++)
+		printf("%13.10f ", U_n[i][j]);
+	printf("\n");
+}
 
 free(U);
 free(U_n);
