8372befff8ef4ffa0f62c0e096a85aae3f75772d
SemyonIlyasov <s.ilyasov@g.nsu.ru>
2022-05-24 03:16:00 +0000

    nccl

--- mpi_cuda_nccl.cu
+++ mpi_cuda_nccl.cu
@@ -6,11 +6,11 @@
 #include <nccl.h>
 // #define NUM_DEVICES 4
 
-void print_matrix_d(double* dst, int numRows, int n)
+void print_matrix_d(double* dst, int num_rows, int n)
 {
-    double *a = (double*)calloc(sizeof(double), numRows * n);
-    cudaMemcpy(a, dst, numRows * n * sizeof(double), cudaMemcpyDeviceToHost);
-    for (int i = 0; i < numRows; ++i)
+    double *a = (double*)calloc(sizeof(double), num_rows * n);
+    cudaMemcpy(a, dst, num_rows * n * sizeof(double), cudaMemcpyDeviceToHost);
+    for (int i = 0; i < num_rows; ++i)
     {
         for (int j = 0; j < n; ++j)
             printf("%lf ", a[i * n + j]);
@@ -51,14 +51,13 @@ __global__ void arr_diff(double* U_n_d, double* U_d, double* U_d_diff, int n, in
 int main(int argc, char * argv[])
 {
 
-    MPI_Status status;
     int local_rank, proc_amount;
 
     MPI_Init(&argc, &argv);
 
-    double min_error = 0.000001;
+    double min_err = 0.000001;
     int N = 128;
-    int iter_max = 50000;
+    int maxi = 50000;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &local_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &proc_amount);
@@ -72,6 +71,9 @@ int main(int argc, char * argv[])
     double *max_d = NULL;
     void *d_temp_storage = NULL;
     size_t temp_storage_bytes = 0;
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
 
    //  cudaSetDevice(local_rank % proc_amount);
 
@@ -88,11 +90,11 @@ int main(int argc, char * argv[])
     int start = ((N / proc_amount) * local_rank) * N;
     int end = (N / proc_amount * (local_rank + 1) + (N % proc_amount) * isLastProcFlag) * N; // [start; end)
 
-    int numElems = end - start;
-    int numRows = numElems / N;
+    int num_elems = end - start;
+    int num_rows = num_elems / N;
 
     // interpolation for different processes
-    tmp = (double*)calloc(numElems, sizeof(double));
+    tmp = (double*)calloc(num_elems, sizeof(double));
 
     double step = (double)(20.0 - 10.0) / ((double)N - 1.0);
     if (isFirstProcFlag)
@@ -108,38 +110,38 @@ int main(int argc, char * argv[])
     if (isLastProcFlag)
     {
         // interpolate lower boarder
-        tmp[(numRows - 1) * N] = 20.0;
-        tmp[numRows * N - 1] = 30.0;
-	// [numRows - 1][127] == [(numRows - 1)*N + (N - 1)] == [numRows * N - 1]
-        for (int i = (numRows - 1) * N + 1; i < numRows * N - 1; i++)
+        tmp[(num_rows - 1) * N] = 20.0;
+        tmp[num_rows * N - 1] = 30.0;
+	// [num_rows - 1][127] == [(num_rows - 1)*N + (N - 1)] == [num_rows * N - 1]
+        for (int i = (num_rows - 1) * N + 1; i < num_rows * N - 1; i++)
             tmp[i] = tmp[i - 1] + step;
     }
 
     // interpolate left and right boarders
-    for (int i = 0 + isFirstProcFlag; i < numRows - isLastProcFlag; i++) {
+    for (int i = 0 + isFirstProcFlag; i < num_rows - isLastProcFlag; i++) {
         tmp[i * N + 0] = 10.0 + step * ((start/ N) + i);
         tmp[i * N + (N - 1)] = 20.0 + step * ((start/ N) + i);
     }
 
     // copying to GPU
-    cudaMalloc((void **)&U_d, (numElems + 2 * N) * sizeof(double));
-    cudaMalloc((void **)&U_n_d, (numElems + 2 * N) * sizeof(double));
-    double* zeros = (double*)calloc(numElems + 2*N, sizeof(double));
+    cudaMalloc((void **)&U_d, (num_elems + 2 * N) * sizeof(double));
+    cudaMalloc((void **)&U_n_d, (num_elems + 2 * N) * sizeof(double));
+    double* zeros = (double*)calloc(num_elems + 2*N, sizeof(double));
     // fill arrays by zeros to clear first and last buff rows
-    cudaMemcpy(U_d, zeros,(numElems + 2*N) * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(U_n_d, zeros, (numElems + 2*N)* sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(U_d + N, tmp, numElems * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(U_n_d + N, tmp, numElems * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(U_d, zeros,(num_elems + 2*N) * sizeof(double), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(U_n_d, zeros, (num_elems + 2*N)* sizeof(double), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(U_d + N, tmp, num_elems * sizeof(double), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(U_n_d + N, tmp, num_elems * sizeof(double), cudaMemcpyHostToDevice, stream);
 
     free(tmp);
 
     dim3 GS = dim3(16, 16);
-    dim3 BS = dim3(ceil(N / (double)GS.x), ceil((numRows + 2) / (double)GS.y));
+    dim3 BS = dim3(ceil(N / (double)GS.x), ceil((num_rows + 2) / (double)GS.y));
 
-    cudaMalloc(&tmp_d, sizeof(double) * numElems);
+    cudaMalloc(&tmp_d, sizeof(double) * num_elems);
     cudaMalloc(&max_d, sizeof(double));
 
-    cub::DeviceReduce::Max(d_temp_storage, temp_storage_bytes, tmp_d, max_d, numRows * N);
+    cub::DeviceReduce::Max(d_temp_storage, temp_storage_bytes, tmp_d, max_d, num_rows * N, stream);
     cudaMalloc(&d_temp_storage, temp_storage_bytes);
 
     int topProcess = (local_rank + 1) % proc_amount;
@@ -148,9 +150,9 @@ int main(int argc, char * argv[])
 
     // calc of values that boundaries of  process
     int y_start = isFirstProcFlag;
-    int y_end = numRows + 1 - isLastProcFlag;
+    int y_end = num_rows + 1 - isLastProcFlag;
 
-    int iter = 0;
+    int it = 0;
     double error = 228;
     //double local_error = 0;
     double * err_d;
@@ -159,29 +161,29 @@ int main(int argc, char * argv[])
     ncclComm_t com;
     ncclCommInitRank(&com, proc_amount, nccl_id, local_rank);
 
-    while (error > min_error && iter < iter_max)
+    while (error > min_err && it < maxi)
     {
-        iter += 1;
+        it += 1;
 
         ncclGroupStart();
-        ncclSend(A_d + matrix_size, matrix_size, ncclDouble, bottomProcess, comm, stream);
-        ncclSend(A_d + numElems, matrix_size, ncclDouble, topProcess, comm, stream);
+        ncclSend(U_d + N, N, ncclDouble, bottomProcess, com, stream);
+        ncclSend(U_d + num_elems, N, ncclDouble, topProcess, com, stream);
 
-        ncclRecv(A_d + numElems + matrix_size, matrix_size, ncclDouble, topProcess, comm, stream);
-        ncclRecv(A_d, matrix_size, ncclDouble, bottomProcess, comm, stream);
+        ncclRecv(U_d + num_elems + N, N, ncclDouble, topProcess, com, stream);
+        ncclRecv(U_d, N, ncclDouble, bottomProcess, com, stream);
         ncclGroupEnd();
 
-        five_point_model_calc<<<BS, GS>>>(U_n_d, U_d, N, y_start, y_end);
-        if (iter % 100 == 0)
+        five_point_model_calc<<<BS, GS,0, stream>>>(U_n_d, U_d, N, y_start, y_end);
+        if (it % 100 == 0)
         {
-            arr_diff<<<BS, GS>>>(U_n_d, U_d, tmp_d, N, y_start, y_end);
-            cub::DeviceReduce::Max(d_temp_storage, temp_storage_bytes, tmp_d, max_d, numRows * N);
+            arr_diff<<<BS, GS, 0, stream>>>(U_n_d, U_d, tmp_d, N, y_start, y_end);
+            cub::DeviceReduce::Max(d_temp_storage, temp_storage_bytes, tmp_d, max_d, num_rows * N, stream);
 
             ncclAllReduce(max_d, err_d, 1, ncclDouble, ncclMax, com, stream);
             cudaMemcpyAsync(&error, err_d, sizeof(double), cudaMemcpyDeviceToHost, stream);
             
             if (isFirstProcFlag)
-                printf("iter: %d error: %e\n", iter, error);
+               printf("iter: %d error: %e\n", it, error);
         }
 
         double *tmp = U_d;
@@ -190,12 +192,14 @@ int main(int argc, char * argv[])
     }
     //if(isLastProcFlag)
     //printf("rank: %d\n", local_rank);
-    //print_matrix_d(U_d, numRows, N);
+    //print_matrix_d(U_d, num_rows, N);
     cudaFree(U_d);
     cudaFree(U_n_d);
     cudaFree(tmp_d);
     cudaFree(max_d);
 
+    cudaStreamDestroy(stream);
+    ncclCommDestroy(com);
     MPI_Finalize();
 
     return 0;
